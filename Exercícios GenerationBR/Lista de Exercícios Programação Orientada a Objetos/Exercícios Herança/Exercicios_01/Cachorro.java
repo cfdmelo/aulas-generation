243a6074e947213a638aa39e31a8864aaeae5198
Camila Freire <cfdmelo@gmail.com>
2022-07-13 15:58:46 -0300

    Exercícios POO - Herança

---
+++ Exercícios GenerationBR/Lista de Exercícios Programação Orientada a Objetos/Exercícios Herança/Exercicios_01/Cachorro.java
@@ -0,0 +1,11 @@
+package POO2;
+
+public class Cachorro extends Animal
+{
+	private String correr;
+	
+	public void correr()
+	{
+		System.out.println("Correndo...");
+	}
+}
---
+++ Exercícios GenerationBR/Lista de Exercícios Programação Orientada a Objetos/Exercícios Herança/Exercicios_01/Cavalo.java
@@ -0,0 +1,5 @@
+package POO2;
+
+public class Cavalo extends Cachorro {
+
+}
