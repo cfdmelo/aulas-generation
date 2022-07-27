97806668dcf9416059a37e65cbb288e755a3fd8e
Camila Freire <cfdmelo@gmail.com>
2022-07-26 21:51:06 -0300

    Exercícios 5

---
+++ Exercícios GenerationBR/Lista de Exercícios em MySQL/Exercício5/Inserto_into_tb_classes.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_classes (classe_arqueiro, classe_guerreiro, classe_mago) VALUES (true, false, false);
+INSERT INTO tb_classes (classe_arqueiro, classe_guerreiro, classe_mago) VALUES (false, true, false);
+INSERT INTO tb_classes (classe_arqueiro, classe_guerreiro, classe_mago) VALUES (false, false, true);
+INSERT INTO tb_classes (classe_arqueiro, classe_guerreiro, classe_mago) VALUES (true, true, true);
+INSERT INTO tb_classes (classe_arqueiro, classe_guerreiro, classe_mago) VALUES (false, false, false);
---
+++ Exercícios GenerationBR/Lista de Exercícios em MySQL/Exercício5/Select_innerjoin_tables.sql
@@ -0,0 +1,2 @@
+SELECT * FROM tb_personagem INNER JOIN tb_classes 
+ON tb_classes.id = tb_personagem.classes_id
