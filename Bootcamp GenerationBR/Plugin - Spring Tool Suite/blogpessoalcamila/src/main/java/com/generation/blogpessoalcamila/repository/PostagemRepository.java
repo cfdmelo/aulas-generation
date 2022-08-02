de4b05487c5a9922a212e90c1d43091238f9b58d
Camila Freire <cfdmelo@gmail.com>
2022-08-02 00:14:14 -0300

    API BlogPessoal

--- Bootcamp GenerationBR/Plugin - Spring Tool Suite/blogpessoalcamila/src/main/java/com/generation/blogpessoalcamila/repository/PostagemRepository.java
+++ Bootcamp GenerationBR/Plugin - Spring Tool Suite/blogpessoalcamila/src/main/java/com/generation/blogpessoalcamila/repository/PostagemRepository.java
@@ -1,15 +1,15 @@
 package com.generation.blogpessoalcamila.repository;
 
-import com.generation.blogpessoalcamila.model.PostagemModel;
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.generation.blogpessoalcamila.model.PostagemModel;
+
 @Repository
 public interface PostagemRepository extends JpaRepository<PostagemModel, Long>{
-	public List<PostagemModel> findAllByTituloContainingIgnoreCase (String titulo);
+	public List<PostagemModel> findAllByTituloContainingIgnoreCase(String titulo);
 	
 
 
