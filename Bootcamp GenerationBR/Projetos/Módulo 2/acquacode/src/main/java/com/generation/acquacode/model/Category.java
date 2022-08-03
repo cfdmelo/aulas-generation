41df5adbbe47db8798015ce3927f41748998e917
Camila Freire <cfdmelo@gmail.com>
2022-08-03 12:12:39 -0300

    Criando Camada Model e Classe Categoria

--- Bootcamp GenerationBR/Projetos/Módulo 2/acquacode/src/main/java/com/generation/acquacode/model/Category.java
+++ Bootcamp GenerationBR/Projetos/Módulo 2/acquacode/src/main/java/com/generation/acquacode/model/Category.java
@@ -11,6 +11,7 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "Category")
 public class Category {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
