7dc4b0a10b82c238d8aeccf5c066ea02acf61d80
Camila Freire <cfdmelo@gmail.com>
2022-08-03 12:08:36 -0300

    Framework STS

---
+++ Bootcamp GenerationBR/Projetos/Módulo 2/acquacode/src/main/java/com/generation/acquacode/model/Category.java
@@ -0,0 +1,50 @@
+package com.generation.acquacode.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "Category")
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "You must enter category type")
+	@Size(min = 5, max = 20)
+	private String CategoryType;
+
+	@NotNull(message = "You must enter Brand")
+	@Size(min = 5, max = 20)
+	private String Brand;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCategoryType() {
+		return CategoryType;
+	}
+
+	public void setCategoryType(String categoryType) {
+		CategoryType = categoryType;
+	}
+
+	public String getBrand() {
+		return Brand;
+	}
+
+	public void setBrand(String brand) {
+		Brand = brand;
+	}
+
+}
